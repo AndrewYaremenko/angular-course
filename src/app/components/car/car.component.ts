2412bf76aa9e7fe72edadccfea0b357ccc296f47
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-28 21:22:09 +0300

    properties, methods

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements OnInit {
 
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  optins: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Audi'
+    this.model = 'RS8';
+    this.speed = 235;
+    this.colors = {
+      car: 'white',
+      salon: 'black',
+      wheels: 'gray',
+    };
+    this.optins = [
+      "ABS", "Autopilot", "Autoparking"
+    ];
+  }
+
+}
+
+interface Colors {
+  car: string,
+  salon: string,
+  wheels: string,
 }
