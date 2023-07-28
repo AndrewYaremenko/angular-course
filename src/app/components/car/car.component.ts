675a251aa53190d412c5978cf3f585379b007967
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-28 21:54:48 +0300

    HTML events

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -11,7 +11,7 @@ export class CarComponent implements OnInit {
   speed: number;
   model: string;
   colors: Colors;
-  optins: string[];
+  options: string[];
 
   constructor() { }
 
@@ -24,11 +24,50 @@ export class CarComponent implements OnInit {
       salon: 'black',
       wheels: 'gray',
     };
-    this.optins = [
+    this.options = [
       "ABS", "Autopilot", "Autoparking"
     ];
   }
 
+  carSelect(carName: string): void {
+    if (carName === 'BMW') {
+      this.name = 'BMW'
+      this.model = 'M5';
+      this.speed = 219;
+      this.colors = {
+        car: 'white',
+        salon: 'black',
+        wheels: 'gray',
+      };
+      this.options = [
+        "ABS", "Autopilot", "Autoparking"
+      ];
+    } else if (carName === 'Audi') {
+      this.name = 'Audi'
+      this.model = 'RS8';
+      this.speed = 235;
+      this.colors = {
+        car: 'gray',
+        salon: 'white',
+        wheels: 'black',
+      };
+      this.options = [
+        "Autopilot", "Autoparking"
+      ];
+    } else if (carName === 'Mersedes') {
+      this.name = 'Mersedes'
+      this.model = 'C180';
+      this.speed = 231;
+      this.colors = {
+        car: 'green',
+        salon: 'white',
+        wheels: 'gray',
+      };
+      this.options = [
+        "ABS", "Autopilot"
+      ];
+    }
+  }
 }
 
 interface Colors {
